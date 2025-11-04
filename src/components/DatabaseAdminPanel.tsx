7adf28658b7acaa5d863cb77ecb53eec943a40dd
AmrAtari <atary92@gmail.com>
2025-11-05 00:33:53 +0400

    Create DatabaseAdminPanel.tsx

---
+++ src/components/DatabaseAdminPanel.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DatabaseAdminPanel: React.FC = () => {
+  return (
+    <div>
+      <h2>Database Admin Panel</h2>
+      {/* Add your database admin controls/UI here */}
+    </div>
+  );
+};
+
+export default DatabaseAdminPanel;
