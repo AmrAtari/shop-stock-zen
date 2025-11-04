aef0a323a47957d5a555cd7fd429b2dae3d32a4d
AmrAtari <atary92@gmail.com>
2025-11-05 00:31:43 +0400

    Create WorkflowRules.tsx

---
+++ src/components/WorkflowRules.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WorkflowRules: React.FC = () => {
+  return (
+    <div>
+      <h2>Workflow Rules Component</h2>
+      {/* Add your workflow rules UI here */}
+    </div>
+  );
+};
+
+export default WorkflowRules;
