92dd19fb6cfc3aff2b538ec8c1f5b8349f2e9bce
AmrAtari <atary92@gmail.com>
2025-11-04 23:09:26 +0400

    Update OrganizationalStructureSection.tsx

--- src/sections/OrganizationalStructureSection.tsx
+++ src/sections/OrganizationalStructureSection.tsx
@@ -1,1 +1,7 @@
+import React from "react";
+import OrganizationalStructure from "@/components/OrganizationalStructure";
+
+export default function OrganizationalStructureSection() {
+  return <OrganizationalStructure dynamicCurrencies={[]} />;
+}
 
