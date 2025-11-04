8e9b84135d8a74b38aaadd89c8344bef7fe54f09
AmrAtari <atary92@gmail.com>
2025-11-05 00:33:00 +0400

    Create OrganizationalStructure.tsx

---
+++ src/components/OrganizationalStructure.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OrganizationalStructure: React.FC = () => {
+  return (
+    <div>
+      <h2>Organizational Structure</h2>
+      {/* Add your organizational structure UI here */}
+    </div>
+  );
+};
+
+export default OrganizationalStructure;
