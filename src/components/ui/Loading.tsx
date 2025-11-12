c38428174b4f1e9946ab8cbeace3487a01ebf4d1
AmrAtari <atary92@gmail.com>
2025-11-12 16:20:25 +0400

    Create Loading.tsx

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loading: React.FC = () => (
+  <div className="flex items-center justify-center h-screen">
+    <span className="text-gray-500 text-lg">Loading...</span>
+  </div>
+);
+
+export default Loading;
